5fdcc872c4b4873468fdfd1608b20031cbfc2de7
rst9197 <rahulcdacacts@gmail.com>
2020-07-30 09:44:53 +0530

    Wxpython

---
+++ creating_fileDialog.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import wx
+
+def onButton(event):
+    print("Button pressed.")
+
+app = wx.App()
+frame = wx.Frame(None, -1, 'win.py')
+frame.SetDimensions(0,0,200,50)
+
+# Create open file dialog
+openFileDialog = wx.FileDialog(frame, "Open", "", "","Python files (*.py)|*.py", wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+openFileDialog.ShowModal()
+print(openFileDialog.GetPath())
+openFileDialog.Destroy()
